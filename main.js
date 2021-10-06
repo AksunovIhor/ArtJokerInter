c0a47a5441e0e26e22649024fc76044fd4e46ab2
Ihor Aksunov <Aksunovihor@gmail.com>
2021-10-06 17:55:52 +0300

    added 15-17 + bubble sort

--- main.js
+++ main.js
@@ -1,51 +1,65 @@
 'use strict';
 
-let str1 = "rga";
-let str2 = "agr";
-
-function bubleSort(variable){
-    let sortArray = variable.toLowerCase().split();
-    for(let i = 0; i <= sortArray.length; i++){
-        if(sortArray[i] > sortArray[i + 1]){
-            let curretNumber = sortArray[i];
-            let nextNumber = sortArray[i + 1];
-            sortArray[i] = nextNumber;
-            sortArray[i + 1] = curretNumber;    
-        }
-    } 
-}
-
-//1) анаграмма
-function anogramm(firstWord, secondWord){
-    if(firstWord.length !== secondWord.length){
+//1) Anagramma
+function isAnagramma(firstWord, secondWord) {
+    if (firstWord.length !== secondWord.length) {
         return false;
-    }
-    for(let i = 0; i <= firstWord.length; i++){
-        let c = firstWord[i];
+    };
+
+    for (let i = 0; i < firstWord.length; i++) {
+        let char1 = word1[i];
         let counter1 = 0;
         let counter2 = 0;
-
-        for(let j = 0; j <= firstWord.length; j++){
-            let b = firstWord[j];
-            if(c === b){
-                counter1++
-            }
-            b = secondWord[j];
-            if(c === b){
+        for (let j = 0; j < secondWord.length; j++) {
+            let char2 = firstWord[j];
+            if (char1 === char2) {
+                counter1++;
+            };
+            char2 = secondWord[j];
+            if (char1 === char2) {
                 counter2++;
-            }
-            if(counter1 === counter2){
-                return true;
-            }
-        }
-    }
-};
-anogramm(str1,str2);
+            };
 
-//количество цифр в числе
-function numberCount(number){
-    let numberToString
+        };
+
+        if (counter1 !== counter2) {
+            return false;
+        };
+    };
+    return true;
+};
+//3) количество цифр в числе
+function numbersCount(number){
+    let numeral = 0;
+    let counter = 0;
+    while(number){
+        numeral = number % 10;
+        number = (number - numeral) / 10;
+        if(numeral == number){
+            counter++;
+        }
+        console.log(`${numeral} and ${counter}`);
+    }
 }
+
+/*function getSumNumber(num) {
+        let sum = 0, tmp = 0;
+        while (num) {
+            tmp = num % 10;
+            num = (num - tmp) / 10;
+            sum += tmp;
+            console.log(num);
+        }
+        
+    }
+    alert(getSumNumber(2610));
+
+function sds(number){
+    let tmp = number % 10;
+    //number = (number - tmp) / 10;
+    console.log(tmp);
+}
+sds(123);*/
 numberCount(123);
 
 //4) уникальные слова в предложении
@@ -59,7 +73,7 @@ numberCount(123);
                 counter++;
             }
         }
-        console.log(`В строке ${counter} уникальных слов.`);
+        return counter;
     }
     unicWords(str);
 
@@ -70,15 +84,14 @@ function wordsCounter(string){
     let word = wordsArray[0];
     let counter = 0;
     console.log(wordsArray);
-    for(let j = 0; j <= wordsArray.length; j++){
+    for(let j = 0; j <= wordsArray.length - 1; j++){
         word = wordsArray[j];
-        for(let i = 0; i <= wordsArray.length; i++){
+        for(let i = 0; i <= wordsArray.length - 1; i++){
             if(word == wordsArray[i]){
                 counter++;
             }
         }
     }
-    console.log(`${word} in str = ${counter}`);
 }
 wordsCounter(someStr);
 
@@ -93,18 +106,21 @@ function fibonacciNumbers(counter){
 fibonacciNumbers(5);
 
 //7)площадь и периметр круга, треугольника, квадрата 
-class Circle{
-    cunstructor(radius, mail);
-}
-class Rectangle{
-
+let rectangle = {
+    width : 30,
+    height: 40,
+    
 }
 
 //8) факториал
 function getFactorial(number){
-    if(number == 0 || number == 1) return number;
-    else return number * getFactorial(number - 1);
-};
+    if(number == 0 || number == 1) {
+        return number;
+    }
+    else {
+        return number * getFactorial(number - 1);
+    }
+}
 getFactorial(5);
 
 //9) array element work
@@ -244,37 +260,48 @@ let dbm = new doubleArray(matrixGenerator(3,3));
 dbm.getSumMatrixEllem;
 
 //13) summ min for max
-let arrMinMax = [4,3,4,5,12,3,5,6,71,2,3,4];
-function sumMinForMax(array, minNumber, maxNumber){
+function sumMinForMax(minNumber, maxNumber){
     let sum = 0;
-    for(let i = minNumber; i <= maxNumber; i++){
-        sum += array[i];
+    if(minNumber >= maxNumber){
+        return false;
     }
-    return sum;
-}
-sumMinForMax(arrMinMax, 4, 10);
-
-function sumMinForMaxMultipleThree(array, minNumber, maxNumber){
-    let sum = 0;
-    for(let i = minNumber; i <= maxNumber; i++){
-        if(array[i] % 3 == 0){
-            sum += array[i];
+    else{
+        for(let i = minNumber; i <= maxNumber; i++){
+            sum += i;
         }
+        return sum;
     }
-    return sum;
 }
-sumMinForMaxMultipleThree(arrMinMax, 4, 10);
 
-function sumMinForMaxMultipleThree(array, minNumber, maxNumber){
+function sumMinForMaxMultipleThree(minNumber, maxNumber){
     let sum = 0;
-    for(let i = minNumber; i <= maxNumber; i++){
-        if(array[i] > 0){
-            sum += array[i];
-        }
+    if(minNumber >= maxNumber){
+        return false;
     }
-    return sum;
+    else{
+        for(let i = minNumber; i <= maxNumber; i++){
+            if(i % 3 == 0){
+                sum += i;
+            }
+        }
+        return sum;
+    }
 }
-sumMinForMaxMultipleThree(arrMinMax, 4, 10);
+
+function sumMinForMaxPositive(minNumber, maxNumber){
+    let sum = 0;
+    if(minNumber >= maxNumber){
+        return false;
+    }
+    else{
+        for(let i = minNumber; i <= maxNumber; i++){
+            if(i > 0){
+                sum += i;
+            }
+        }
+        return sum;
+    }
+}
 
 //14) average arr and double arr
 let averArr = [1,2,3,4,5,6,7,8,9,10];
@@ -384,15 +411,99 @@ function getEvenDoubleArrAverage(stroke, collumn){
 getEvenDoubleArrAverage(3,3);
 
 //15) transport matrix
-function matrixTransport(stroke, collumn){
-    let matrix = [];
-    for(let i = 0; i <= stroke-1; i++){
-        matrix[i] = [];
-        for(let j = 0; j <= collumn-1; j++){
-            matrix[i][j] = Math.floor(Math.random() * 10);
-            
+function matrixTransport(matrix){
+
+    let m = matrix.length;
+    let n = matrix[0].length;
+    let transpMatrix = [];
+
+    if(matrix.length === 1 || matrix.length === 0){
+        return matrix;
+    }
+
+    for(let i = 0; i < n; i++){
+        transpMatrix[i] = [];
+        for(let j = 0; j < m; j++){
+            transpMatrix[i][j] = matrix[j][i];
+        }
+    }
+
+    return transpMatrix;
+}
+
+//16 summ matrix
+function summMatrix(firstMatrix, secondMatrix){
+    let m = firstMatrix.length;
+    let n = firstMatrix[0].length;
+    let newMatrix = [];
+    if(firstMatrix.length > secondMatrix.length || secondMatrix.length > firstMatrix.length){
+        return false;
+    }
+    else{
+        for(let i = 0; i < m; i++){
+            newMatrix[i] = [];
+            for(let j = 0; j < n; j++){
+                newMatrix[i][j] = firstMatrix[i][j] + secondMatrix[i][j];
+            }
+        }
+        return newMatrix;
+    }
+}
+
+let testMatrix = [
+    [1,2,3],
+    [4,5,6],
+    [7,8,0]
+]
+//17) delete string with 0
+function dellMatrixStringWithZero(matrix){
+    let m = matrix.length;
+    for(let i = 0; i < m; i++){
+        for(let j = 0; j < m; j++){
+            if(matrix[i][j] == 0){
+                matrix[i][j] == 1;
+            }
         }
     }
     console.log(matrix);
 }
-matrixTransport(3,3);
+dellMatrixStringWithZero(testMatrix);
+
+
+
+//рекурсии
+/*function f(min, max){
+    let sum = 0;
+    if(++min <= max){
+        sum += f(min, max) + min;
+    }
+    return sum;
+}
+f(0, 10);*/
+
+/*function fr(arr, index){
+    index = index || 0;
+    let s = 0;
+    if(index < arr.length){
+        s = arr[index] + fr(arr, ++index);
+    }
+    return s;
+}
+fr([1,2,3,4,5,6,7,8,9]);*/
+
+
+function bubbleSort(array){
+    if(array instanceof Array){
+        for(let i = 0; i < array.length; i++){
+            for(let j = 0; j < array.length; j++){
+                if(array[j] >= array[j + 1]){
+                    let curretNumber = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = curretNumber;
+                }
+            }
+        }
+        return array;
+    }
+}
+bubbleSort([1,5,3,6,7,4,3]);
